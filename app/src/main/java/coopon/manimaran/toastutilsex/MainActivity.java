f437ecdffe6a881c0120df5aa94f1129b512290a
Manimaran <manimarankumar96@gmail.com>
2019-06-25 18:33:24 +0530

    Toast Utils all are done
    Normal Toast - Short, Long, Custom
    Icon - Info, Warning, Success, Error added.
    Short Toast showing short period of time.
    Long Toast showing long period of time.
    Custom Toast showing user specific time period.
    
    Info - Showing Information to User.
    Success - Showing successful information to user.
    Warning - Showing alert information to user.
    Error - Showing Error information to user.

--- app/src/main/java/coopon/manimaran/toastutilsex/MainActivity.java
+++ app/src/main/java/coopon/manimaran/toastutilsex/MainActivity.java
@@ -1,36 +1,64 @@
 package coopon.manimaran.toastutilsex;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import coopon.manimaran.toastutils.ToastUtils;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnNormal, btnShort, btnLong, btnInfo, btnSuccess, btnWarning, btnError;
+    Button btnShort, btnLong, btnCustom;
+    Button btnInfoShort, btnSuccessShort, btnWarningShort, btnErrorShort;
+    Button btnInfoLong, btnSuccessLong, btnWarningLong, btnErrorLong;
+    Button btnInfoCustom, btnSuccessCustom, btnWarningCustom, btnErrorCustom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnNormal = findViewById(R.id.btn_normal);
         btnShort = findViewById(R.id.btn_short);
         btnLong = findViewById(R.id.btn_long);
-        btnInfo = findViewById(R.id.btn_info);
-        btnSuccess = findViewById(R.id.btn_success);
-        btnWarning = findViewById(R.id.btn_warning);
-        btnError = findViewById(R.id.btn_error);
+        btnCustom = findViewById(R.id.btn_custom);
 
-        btnNormal.setOnClickListener(this);
+        btnInfoShort = findViewById(R.id.btn_info_short);
+        btnSuccessShort = findViewById(R.id.btn_success_short);
+        btnWarningShort = findViewById(R.id.btn_warning_short);
+        btnErrorShort = findViewById(R.id.btn_error_short);
+
+        btnInfoLong = findViewById(R.id.btn_info_long);
+        btnSuccessLong = findViewById(R.id.btn_success_long);
+        btnWarningLong = findViewById(R.id.btn_warning_long);
+        btnErrorLong = findViewById(R.id.btn_error_long);
+
+        btnInfoCustom = findViewById(R.id.btn_info_custom);
+        btnSuccessCustom = findViewById(R.id.btn_success_custom);
+        btnWarningCustom = findViewById(R.id.btn_warning_custom);
+        btnErrorCustom = findViewById(R.id.btn_error_custom);
+
         btnShort.setOnClickListener(this);
         btnLong.setOnClickListener(this);
-        btnInfo.setOnClickListener(this);
-        btnSuccess.setOnClickListener(this);
-        btnWarning.setOnClickListener(this);
-        btnError.setOnClickListener(this);
+        btnCustom.setOnClickListener(this);
+
+        btnInfoShort.setOnClickListener(this);
+        btnSuccessShort.setOnClickListener(this);
+        btnWarningShort.setOnClickListener(this);
+        btnErrorShort.setOnClickListener(this);
+
+        btnInfoLong.setOnClickListener(this);
+        btnSuccessLong.setOnClickListener(this);
+        btnWarningLong.setOnClickListener(this);
+        btnErrorLong.setOnClickListener(this);
+
+        btnInfoCustom.setOnClickListener(this);
+        btnSuccessCustom.setOnClickListener(this);
+        btnWarningCustom.setOnClickListener(this);
+        btnErrorCustom.setOnClickListener(this);
     }
 
     @Override
@@ -38,13 +66,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId())
         {
-            case R.id.btn_normal : ToastUtils.showNormal(getApplicationContext(), "Normal"); break;
-            case R.id.btn_short : ToastUtils.showShort(getApplicationContext(), "Short"); break;
-            case R.id.btn_long : ToastUtils.showLong(getApplicationContext(), "Long"); break;
-            case R.id.btn_info : ToastUtils.showInfo(getApplicationContext(), "Info"); break;
-            case R.id.btn_success : ToastUtils.showSuccess(getApplicationContext(), "Success"); break;
-            case R.id.btn_warning : ToastUtils.showWarning(getApplicationContext(), "Warning"); break;
-            case R.id.btn_error : ToastUtils.showError(getApplicationContext(), "Error"); break;
+            case R.id.btn_short : ToastUtils.showShort(getApplicationContext(), "Short Toast"); break;
+            case R.id.btn_long : ToastUtils.showLong(getApplicationContext(), "Long Toast"); break;
+            case R.id.btn_custom : ToastUtils.showCustom(getApplicationContext(), "Custom Toast for 3sec", 3000); break;
+
+            case R.id.btn_info_short : ToastUtils.showInfoShort(getApplicationContext(), "Info Toast - Short"); break;
+            case R.id.btn_success_short : ToastUtils.showSuccessShort(getApplicationContext(), "Success Toast - Short"); break;
+            case R.id.btn_warning_short : ToastUtils.showWarningShort(getApplicationContext(), "Warning Toast - Short"); break;
+            case R.id.btn_error_short : ToastUtils.showErrorShort(getApplicationContext(), "Error Toast - Short"); break;
+
+            case R.id.btn_info_long : ToastUtils.showInfoLong(getApplicationContext(), "Info Toast - Long"); break;
+            case R.id.btn_success_long : ToastUtils.showSuccessLong(getApplicationContext(), "Success Toast - Long"); break;
+            case R.id.btn_warning_long : ToastUtils.showWarningLong(getApplicationContext(), "Warning Toast - Long"); break;
+            case R.id.btn_error_long : ToastUtils.showErrorLong(getApplicationContext(), "Error Toast - Long"); break;
+
+            case R.id.btn_info_custom : ToastUtils.showInfoCustom(getApplicationContext(), "Info Toast - Custom for 3sec", 3000); break;
+            case R.id.btn_success_custom : ToastUtils.showSuccessCustom(getApplicationContext(), "Success Toast - Custom for 3sec", 3000); break;
+            case R.id.btn_warning_custom : ToastUtils.showWarningCustom(getApplicationContext(), "Warning Toast - Custom for 3sec", 3000); break;
+            case R.id.btn_error_custom : ToastUtils.showErrorCustom(getApplicationContext(), "Error Toast - Custom for 3sec", 3000); break;
         }
 
     }
--- toastutils/src/main/java/coopon/manimaran/toastutils/ToastUtils.java
+++ toastutils/src/main/java/coopon/manimaran/toastutils/ToastUtils.java
@@ -1,11 +1,21 @@
 package coopon.manimaran.toastutils;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
+
+import static androidx.core.view.ViewCompat.setBackground;
 
 public class ToastUtils {
 
@@ -15,62 +25,134 @@ public class ToastUtils {
     private static final int NORMAL = 3;
     private static final int INFO = 4;
 
+
     public static void showToast(Context context, String msg, int duration, int type) {
-        Toast toast = Toast.makeText(context, msg, duration);
-        View view = toast.getView();
 
         int bgColor = R.color.toast_normal;
+        int icon = R.drawable.ic_success;
         switch (type) {
             case NORMAL:
                 bgColor = R.color.toast_normal;
+                icon = 0;
                 break;
             case ERROR:
                 bgColor = R.color.toast_error;
+                icon = R.drawable.ic_error;
                 break;
             case WARNING:
                 bgColor = R.color.toast_warning;
+                icon = R.drawable.ic_warning;
                 break;
             case SUCCESS:
                 bgColor = R.color.toast_success;
+                icon = R.drawable.ic_success;
                 break;
             case INFO:
                 bgColor = R.color.toast_info;
+                icon = R.drawable.ic_info;
                 break;
         }
-        //Gets the actual oval background of the Toast then sets the colour filter
-        view.getBackground().setColorFilter(ContextCompat.getColor(context, bgColor), PorterDuff.Mode.SRC_IN);
 
-        //Gets the TextView from the Toast so it can be editted
-        //TextView text = view.findViewById(android.R.id.message);
-        //text.setTextColor(YOUR_TEXT_COLOUR);
-        toast.show();
+        // Toast view
+        Toast currentToast = Toast.makeText(context, "", duration);
+        View toastLayout = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_toast, null);
+        ImageView toastIcon = toastLayout.findViewById(R.id.toast_icon);
+        TextView toastTextView = toastLayout.findViewById(R.id.toast_text);
+
+        //Gets the actual oval background of the Toast then sets the colour filter
+        toastLayout.getBackground().setColorFilter(ContextCompat.getColor(context, bgColor), PorterDuff.Mode.SRC_IN);
+
+        if(icon != 0)
+            toastIcon.setImageResource(icon);
+        else
+            toastIcon.setVisibility(View.GONE);
+
+        toastTextView.setText(msg);
+        toastTextView.setTextColor(Color.WHITE);
+        toastTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        currentToast.setView(toastLayout);
+
+        // Show Toast
+        currentToast.show();
+
+    }
+
+    /**
+     * Normal
+     */
+
+    public static void showShort(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_SHORT, NORMAL);
     }
 
     public static void showLong(Context context, String msg) {
         showToast(context, msg, Toast.LENGTH_LONG, NORMAL);
     }
 
-    public static void showShort(Context context, String msg) {
-        showToast(context, msg, Toast.LENGTH_SHORT, NORMAL);
+    public static void showCustom(Context context, String msg, int duration) {
+        showToast(context, msg, duration, NORMAL);
     }
 
-    public static void showError(Context context, String msg) {
-        showToast(context, msg, Toast.LENGTH_LONG, ERROR);
+
+    /**
+     * Short
+     */
+
+    public static void showInfoShort(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_SHORT, INFO);
     }
 
-    public static void showWarning(Context context, String msg) {
-        showToast(context, msg, Toast.LENGTH_LONG, WARNING);
+    public static void showSuccessShort(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_SHORT, SUCCESS);
     }
 
-    public static void showSuccess(Context context, String msg) {
-        showToast(context, msg, Toast.LENGTH_LONG, SUCCESS);
+    public static void showWarningShort(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_SHORT, WARNING);
     }
 
-    public static void showInfo(Context context, String msg) {
+    public static void showErrorShort(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_SHORT, ERROR);
+    }
+
+
+    /**
+     * Long
+     */
+
+    public static void showInfoLong(Context context, String msg) {
         showToast(context, msg, Toast.LENGTH_LONG, INFO);
     }
 
-    public static void showNormal(Context context, String msg) {
-        showToast(context, msg, Toast.LENGTH_LONG, NORMAL);
+    public static void showSuccessLong(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_LONG, SUCCESS);
     }
+
+    public static void showWarningLong(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_LONG, WARNING);
+    }
+
+    public static void showErrorLong(Context context, String msg) {
+        showToast(context, msg, Toast.LENGTH_LONG, ERROR);
+    }
+
+    /**
+     * Custom
+     */
+
+    public static void showInfoCustom(Context context, String msg, int duration) {
+        showToast(context, msg, duration, INFO);
+    }
+
+    public static void showSuccessCustom(Context context, String msg,int duration) {
+        showToast(context, msg, duration, SUCCESS);
+    }
+
+    public static void showWarningCustom(Context context, String msg, int duration) {
+        showToast(context, msg, duration, WARNING);
+    }
+
+    public static void showErrorCustom(Context context, String msg, int duration) {
+        showToast(context, msg, duration, ERROR);
+    }
+
 }
